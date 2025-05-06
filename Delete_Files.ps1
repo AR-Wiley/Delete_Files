4e0ae22765b213a1ec7615e8722c119f09569aaf
AR-Wiley <ar.wiley805@outlook.com>
2025-05-05 20:07:00 -0500

    first commit

---
+++ Delete_Files.ps1
@@ -0,0 +1,10 @@
+﻿$date = (Get-Date).AddDays(-365)
+
+$folder_path = "C:\Users\wiley\Desktop\GlobexFiles"
+
+$dated_files = Get-ChildItem -Path $folder_path -File | Where-Object {$_.LastWriteTime -le $date}
+
+for ($i = 0; $i -lt $dated_files.Length; $i++){
+    Remove-Item ($dated_files[$i])
+}
+
